d2ce88df4d5a4981309eb8d83ecbb6418d41f1b4
slavamukhin <via4eslav.mukhin@yandex.ru>
2022-01-01 14:27:37 +0300

    fix layout shop

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-import gulp from 'gulp';
-import htmlmin from 'gulp-htmlmin';
-import fileinclude from 'gulp-file-include';
-import cssmin from 'gulp-cssmin';
-import importCss from 'gulp-import-css';
-import image from 'gulp-image';
-import clean from 'gulp-clean';
-
+import gulp from 'gulp'
+import htmlmin from 'gulp-htmlmin'
+import fileinclude from 'gulp-file-include'
+import cssmin from 'gulp-cssmin'
+import importCss from 'gulp-import-css'
+import image from 'gulp-image'
+import clean from 'gulp-clean'
 
 function html() {
   return gulp
@@ -19,43 +18,49 @@ function html() {
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('build/'))
 }
-gulp.task(html);
+gulp.task(html)
 
 function css() {
-    return gulp.src('assets/css/style.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('build/assets/css'));
+  return gulp
+    .src('assets/css/style.css')
+    .pipe(cssmin())
+    .pipe(gulp.dest('build/assets/css'))
 }
-gulp.task(css);
+gulp.task(css)
 
 function font() {
   return gulp.src('assets/fonts/*.ttf').pipe(gulp.dest('build/assets/fonts'))
 }
 gulp.task(font)
 
-function img() {
-  return gulp.src('assets/img/*')
-  .pipe(image({
-    pngquant: true,
-    optipng: false,
-    zopflipng: true,
-    jpegRecompress: false,
-    mozjpeg: true,
-    gifsicle: true,
-    svgo: true,
-    concurrent: 10,
-    quiet: true,
-  }))
-  .pipe(gulp.dest('build/assets/img'));
+function favicon() {
+  return gulp.src('favicon.png').pipe(gulp.dest('build/'))
 }
-gulp.task(img);
+gulp.task(favicon)
+
+function img() {
+  return gulp
+    .src('assets/img/*')
+    .pipe(
+      image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: true,
+      })
+    )
+    .pipe(gulp.dest('build/assets/img'))
+}
+gulp.task(img)
 
 function deleteBuild() {
   return gulp.src('build', { read: false, allowEmpty: true }).pipe(clean())
 }
 gulp.task(deleteBuild)
 
-gulp.task('start', gulp.series(deleteBuild, html, font, css, img))
-
-
-    
+gulp.task('start', gulp.series(deleteBuild, html, font, favicon, css, img))
